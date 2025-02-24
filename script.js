5ea89b22ade06a2b8e4bfd4657e9d339b4c4141d
sahil-patel620 <Sp453771@gmail.com>
2025-02-24 08:06:38 +0530

    portfolio updated

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+// Typing Effect
 let typed = new Typed("#element", {
     strings: ["Sahil Patel", "Developer"],
     typeSpeed: 100,
     showCursor: false,
   });
 
-const body = document.querySelector("body");
+// Dark theme
+const body = document.querySelector("body")
 const icon = document.querySelector("#icon");
-const logo = document.querySelector(".logo");
+const logo = document.querySelector(".nav-branding");
 const menuBtn = document.querySelector("#menu-icon");
 
-icon.addEventListener("click",  (e) =>  {
-    e.preventDefault();
+icon.addEventListener("click",  () =>  {
   body.classList.toggle("dark-mode");
 
   if (body.classList.contains("dark-mode")) {
@@ -21,3 +22,21 @@ icon.addEventListener("click",  (e) =>  {
     logo.src = "assests/sahil-logo.png";
   }
 });
+
+
+
+// Responsive NavBar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector("header");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    header.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
